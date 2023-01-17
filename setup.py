e7a5ae16184f03d18ab81e9ced2659912a829201
Gennaro Farina <g.farina@almawave.it>
2023-01-17 15:40:09 +0100

    [DIST] update in dependencies for the wheel

--- setup.py
+++ setup.py
@@ -4,4 +4,6 @@ setup(
     name = 'tf_models_official_nlp_only', 
     version='2.8.0', 
     packages=find_packages(),
+    # List project dependencies:
+    install_requires=["gin-config>=0.5.0;python_version>='<3.8>'"],
 )
