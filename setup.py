5adf7f7a6c6cabb1ec518bb08bcd1611dae9e674
Gennaro Farina <g.farina@almawave.it>
2023-01-17 14:51:38 +0100

    [DIST] distributing tf models official NLP only r2.8.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = 'tf_models_official_nlp_only-2.3.0', 
+    name = 'tf_models_official_nlp_only', 
     version='2.8.0', 
     packages=find_packages(),
 )
